013a09e03ed5ff8eb039acc64c911ce4cc6e86d9
saipriyanka.ravindra@gmail.com <saipriyanka.ravindra@gmail.com>
2021-08-24 15:24:24 +0530

    commit

--- jenkisdemo1/src/main/java/com/example/demo/Jenkisdemo1Application.java
+++ jenkisdemo1/src/main/java/com/example/demo/Jenkisdemo1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Jenkisdemo1Application {
 
 	public static void main(String[] args) {
+		System.out.println("welcome");
 		SpringApplication.run(Jenkisdemo1Application.class, args);
 	}
 
